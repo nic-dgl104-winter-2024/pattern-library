491b96fe1b7c08097cb85093f42c37fbbc01bc6a
Abdulrahman295 <movieshome160@gmail.com>
2024-03-16 14:07:30 +0200

    Add references and usage example

--- patterns/singleton/singleton-lazy.java
+++ patterns/singleton/singleton-lazy.java
@@ -1,9 +1,14 @@
 /**
- * Singleton class: Lazy Initialization
+ * DESIGN PATTERN:
+ * Singleton
  * 
- * This class represents a singleton object that is lazily initialized. It ensures that only one instance of the class
- * is created and provides a global point of access to it.
+ * DESCRIPTION:
+ * This class represents a singleton object that is lazily initialized.
+ * It ensures that only one instance of the class is created and provides a global point of access to it.
  * 
+ * REFERENCES:
+ * https://refactoring.guru/design-patterns/singleton
+ * https://www.digitalocean.com/community/tutorials/java-singleton-design-pattern-best-practices-examples
  */
 
 package patterns.singleton;
@@ -15,11 +20,29 @@ class Singleton {
     // Make the constructor private so that this class cannot be instantiated
     private Singleton(){}
     
-    // Provide a public method That returns the singleton instance. If the instance is not yet created, it creates a new instance and returns it.
+    // Provide a public method That returns the singleton instance. 
+    //If the instance is not yet created, it creates a new instance and returns it.
     public static Singleton getInstance(){
         if(instance == null){
             instance = new Singleton();
         }
         return instance;
+    }
+}
+
+/**
+ * USAGE EXAMPLE:
+ * In this example, we demonstrate the usage of the Singleton class by calling the `getInstance()` method twice.
+ * We then compare the two instances to check if they point to the same object in memory.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton instance1 = Singleton.getInstance();
+
+        Singleton instance2 = Singleton.getInstance();
+
+        System.out.println(instance1 == instance2);
+        // Output: true
     }
 }
