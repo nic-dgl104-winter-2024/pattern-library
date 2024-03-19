476a1ad22302ff5adfd206a0a0d0483c6c6d7d17
Yogesh Manni <yogeshmanni786@gmail.com>
2024-03-19 16:23:04 -0700

    Connect to mongoDb database and initialize middlewares

--- patterns/MVC/controllers/server.js
+++ patterns/MVC/controllers/server.js
@@ -8,3 +8,23 @@ const cors = require("cors"); // CORS middleware for handling Cross-Origin Resou
 const todoController = require("./controllers/tasks"); // Todo controller for handling Todo-related requests
 const authController = require("./controllers/authentication"); // User controller for handling User-related requests
 const categoryController = require("./controllers/category"); // Category controller for handling Category-related requests
+
+// Initialize express app
+const app = express();
+
+// Middleware
+// Parse JSON bodies of incoming requests
+app.use(bodyParser.json());
+// Enable CORS for all requests
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/todo_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+// if there is an error while connecting  to DB then print it on console and stop the execution of application
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
