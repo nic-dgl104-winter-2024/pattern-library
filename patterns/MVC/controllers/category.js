9688c5f218ccfd9348b61843e3a26b6216b6e660
Yogesh Manni <yogeshmanni786@gmail.com>
2024-03-19 15:57:50 -0700

    Add method to create a new category

--- patterns/MVC/controllers/category.js
+++ patterns/MVC/controllers/category.js
@@ -14,6 +14,24 @@ const categoryController = {
       res.status(500).json({ message: error.message });
     }
   },
+
+  // Create a new category
+  createCategory: async (req, res) => {
+    // Create a new Category object based on the request body
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    try {
+      // Save the new category to the database
+      const newCategory = await category.save();
+      // Respond with the newly created category
+      res.status(201).json(newCategory);
+    } catch (error) {
+      // If an error occurs, respond with an error message
+      res.status(400).json({ message: error.message });
+    }
+  },
 };
 
 module.exports = categoryController;
