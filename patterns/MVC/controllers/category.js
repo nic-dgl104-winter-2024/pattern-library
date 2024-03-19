a6bf09b120ad07d5e31fd20622e7881bf236d072
Yogesh Manni <yogeshmanni786@gmail.com>
2024-03-19 15:55:31 -0700

    Add method to get all categories from database

--- patterns/MVC/controllers/category.js
+++ patterns/MVC/controllers/category.js
@@ -1,6 +1,19 @@
 const Category = require("../models/categoryModel");
 
 // Controller methods for Category model
-const categoryController = {};
+const categoryController = {
+  // Get all categories
+  getAllCategories: async (req, res) => {
+    try {
+      // Retrieve all categories from the database
+      const categories = await Category.find();
+      // Respond with the list of categories
+      res.json(categories);
+    } catch (error) {
+      // If an error occurs, respond with an error message
+      res.status(500).json({ message: error.message });
+    }
+  },
+};
 
 module.exports = categoryController;
