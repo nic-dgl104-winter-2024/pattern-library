8595e77d4bc6325b73f1516887b97218517864a0
swathysukumaran <swathysukumaran@gmail.com>
2024-03-19 22:07:13 -0700

    Add subscribe  function

--- patterns/observer/observer.js
+++ patterns/observer/observer.js
@@ -9,4 +9,14 @@
 
 class Observable {
 
+    constructor() {
+
+        // The subscribers array keeps the list of subscribers
+        this.subscribers = []
+    }
+
+    // The subscribing function will be added to the list of subscribers
+    subscribe(fn) {
+        this.subscribers.push(fn);
+    }
 }
