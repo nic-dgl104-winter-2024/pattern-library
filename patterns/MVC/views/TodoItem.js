02e48c753312a7b6264f9e2ab72110022a0f58c1
Yogesh Manni <yogeshmanni786@gmail.com>
2024-03-19 23:54:40 -0700

    Add handleUpdate and handleDelete methods to update and delete tasks from the list

--- patterns/MVC/views/TodoItem.js
+++ patterns/MVC/views/TodoItem.js
@@ -1,9 +1,33 @@
 // Import necessary modules
 import React from "react"; // For React
-import api from "../api"; // Import the API service
+import api from "./api"; // Import the API service
 
 // TodoItem component
 function TodoItem({ todo }) {
+  // Function to handle update button click
+  const handleUpdate = async () => {
+    try {
+      // Make API request to update the todo item
+      await api.put(`/todos/${todo._id}`, { completed: !todo.completed });
+      // Show updation message on console
+      console.log("Task updated successfully !!");
+    } catch (error) {
+      console.error("Error updating todo:", error);
+    }
+  };
+
+  // Function to handle delete button click
+  const handleDelete = async () => {
+    try {
+      // Make API request to delete the todo item
+      await api.delete(`/todos/${todo._id}`);
+      // Show delete message on console
+      console.log("Task deleted successfully !!");
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
+  };
+
   // Render individual todo item
   return (
     <li>
