04262ea344eacdc22d9c4c022f823188b74723ce
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2024-03-22 13:47:43 -0700

    Create new Subject class

---
+++ journals/JournalSubject.js
@@ -0,0 +1,25 @@
+// JournalSubject.js
+export default class JournalSubject {
+    constructor() {
+      this.observers = [];
+      this.entries = [];
+    }
+  
+    addObserver(observer) {
+      this.observers.push(observer);
+    }
+  
+    removeObserver(observer) {
+      this.observers = this.observers.filter(obs => obs !== observer);
+    }
+  
+    notifyObservers() {
+      this.observers.forEach(observer => observer.update(this.entries));
+    }
+  
+    addEntry(entry) {
+      this.entries.push(entry);
+      this.notifyObservers();
+    }
+  }
+  
