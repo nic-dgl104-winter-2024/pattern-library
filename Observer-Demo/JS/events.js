4f4237fe2b6160e6d7786d08676fd8d95d357588
Dhuds1 <dhudson1@northislandcollege.ca>
2024-03-23 12:07:08 -0700

    Add function to convert ms to s

--- Observer-Demo/JS/events.js
+++ Observer-Demo/JS/events.js
@@ -4,16 +4,22 @@
 
 function timeControl() {
   event.preventDefault();
-  setTimeDelay();
+  console.log(setTimeDelay());
 }
 function setTimeDelay() {
   const DELAY = window.TIMEDELAY.value;
+  if(isNaN(DELAY)){
+    return (error = "Please enter a number");
+  }
   if(DELAY >= 0 && DELAY <=10000) {
-    return (success = "");
+    return (success = `Delay set to ${convertToSeconds(DELAY)}s`);
   } else {
-    return (error = "");
+    return (error = `${DELAY} is out of range. Please enter a number between 0 and 10,000`);
   }
 }
 function getTimeDelay() {
   return window.TIMEDELAY.value;
+}
+function convertToSeconds(time) {
+  return Number(time / 1000).toFixed(2);
 }
