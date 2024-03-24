97c1fe928e8dd3fe5e9ba9fdf2eaf14628fb5b77
Dhuds1 <dhudson1@northislandcollege.ca>
2024-03-24 13:32:00 -0700

    Add test button

--- Observer-Demo/JS/events.js
+++ Observer-Demo/JS/events.js
@@ -1,25 +1,31 @@
 import * as T from "./controllers/time.js";
 import * as S from "./controllers/storage.js";
-import * as P from  "./controllers/printer.js"
-import {Subject, Observer} from  "./classes/ObserverPattern.js"
+import * as P from "./controllers/printer.js";
+import { Subject, Observer } from "./classes/ObserverPattern.js";
 /**
  * T = time
  * S = storage
  * P = printer
  */
-"use strict";
-window.TIME_FORM = document.getElementById('timeController');
-window.TIME_FORM.addEventListener('submit', T.setTimeDelay);
+("use strict");
+window.TIME_FORM = document.getElementById("timeController");
+window.TIME_FORM.addEventListener("submit", T.setTimeDelay);
 
-window.SUBJECT_FORM = document.getElementById('subjectForm');
-window.SUBJECT_FORM.addEventListener('submit', event => {
+window.SUBJECT_FORM = document.getElementById("subjectForm");
+window.SUBJECT_FORM.addEventListener("submit", (event) => {
   let items = S.createObject(event.target.id);
-  P.createPrintable(items);
+  P.print(items);
 });
 
-window.OBSERVER_FORM = document.getElementById('observerForm');
-window.OBSERVER_FORM.addEventListener('submit', (event) =>{
+window.OBSERVER_FORM = document.getElementById("observerForm");
+window.OBSERVER_FORM.addEventListener("submit", (event) => {
   let items = S.createObject(event.target.id);
   console.log(items);
-  let printList = P.createPrintable(items);
+  P.print(items);
+});
+
+let test = document.getElementById('test');
+test.addEventListener("click", (event) => {
+  let items = S.getObjects("observer");
+  if (items !== null) P.print(items);
 });
