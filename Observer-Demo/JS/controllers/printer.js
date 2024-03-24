6ceb0661fd36781a3e27087a9e2d9efb1dac5711
Dhuds1 <dhudson1@northislandcollege.ca>
2024-03-24 13:55:02 -0700

    Add Subject and Observer print functions

--- Observer-Demo/JS/controllers/printer.js
+++ Observer-Demo/JS/controllers/printer.js
@@ -1,7 +1,47 @@
 import {Subject, Observer} from "../classes/ObserverPattern.js";
 
 export function print(items) {
-  
+  if(items[0] instanceof Observer) {
+    printObservers(items);
+  } else {
+    printSubjects(items);
+  }
+}
+
+function printObservers(items) {
+  const ul = document.getElementById('allObservers');
+  items.forEach(item => {
+    const li = document.createElement('li');
+    li.append(item.name);
+    if(item.subList.length !== 0) {
+      const subUl = document.createElement('ul');
+      items.subList.forEach(sub => {
+        let subLi = document.createElement('li');
+        subLi.setAttribute(`[${item.bind}="${item.id}"]`);
+        subLi.append(sub);
+        subUl.appendChild(subLi);
+      });
+    }
+    ul.appendChild(li);
+  });
+}
+
+function printSubjects(items) {
+  const ul = document.getElementById('allSubjects');
+  items.forEach(item => {
+    const li = document.createElement('li');
+    li.append(item.name);
+    if (item.observerList.length !== 0) {
+      const subUl = document.createElement('ul');
+      items.observerList.foreach(obs => {
+        let subLi = document.createElement('li');
+        subLi.setAttribute(`[${item.bind}="${item.id}"]`);
+        subLi.append(obs);
+        subUl.appendChild(subLi);
+      })
+    }
+    ul.appendChild(li);
+  });
 }
 /**
  * Subject Template
