1f1c120530437df4167a0bacb0ace16642599cc4
kuljit95 <144160996+kuljit95@users.noreply.github.com>
2024-03-24 00:16:43 -0700

    Create interfaces

---
+++ patterns/observers/observer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+// Subject interface
+public interface ISubject
+{
+    void Attach(IObserver observer);
+    void Detach(IObserver observer);
+    void Notify();
+}
+
+// Observer interface
+public interface IObserver
+{
+    void Update(int number);
+}
