e028880a918ad4bc559225941b95b4cccd029ac0
Dhuds1 <dhudson1@northislandcollege.ca>
2024-03-24 13:30:47 -0700

    Add wire of elements in printer

--- Observer-Demo/JS/controllers/printer.js
+++ Observer-Demo/JS/controllers/printer.js
@@ -1,3 +1,26 @@
 import {Subject, Observer} from "../classes/ObserverPattern.js";
-export function createPrintable(items, instance) {
+
+export function print(items) {
+  
 }
+/**
+ * Subject Template
+ * 
+ * 
+ * Subject 1 -- data
+ *  - Subscriber 1
+ *  - Subscriber 2
+ * Subject 2 -- data
+ *  - Subscriber
+ */
+
+/**
+ * Observer Template
+ * 
+ * 
+ * Observer 1
+ *  - Observing -- data
+ *  - Observing -- data
+ * Observer 2
+ *  - Observing -- data
+ */
