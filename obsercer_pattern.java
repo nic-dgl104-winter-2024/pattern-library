ba56954a6503222f4e2e75e504305d771db45cc9
AditiVatish <144161682+AditiVatish@users.noreply.github.com>
2024-03-25 09:35:01 -0700

    Override methods

--- obsercer_pattern.java
+++ obsercer_pattern.java
@@ -16,4 +16,28 @@ class ConcreteSubject implements Subject {
     public int getState() {
         return state;
     }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyObservers();
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
+
 }
