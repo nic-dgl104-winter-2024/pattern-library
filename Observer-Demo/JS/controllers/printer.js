f5604df2fc34bd6aa12ba6f89aa735248fabf39e
Dhuds1 <dhudson1@northislandcollege.ca>
2024-03-25 13:10:18 -0700

    Add separation to reduce doubled code

--- Observer-Demo/JS/controllers/printer.js
+++ Observer-Demo/JS/controllers/printer.js
@@ -3,6 +3,7 @@ export function observer(parent, observers) {
 
   observers.forEach((observer) => {
     const ele = li(observer, "observer");
+    bind(ele, observer);
     if (observer.subList.length !== 0) {
       ele.appendChild(sub(observer.subList));
     }
@@ -10,34 +11,15 @@ export function observer(parent, observers) {
   });
 }
 
-function sub(list) {
-  const ul = document.createElement("ul");
-  list.forEach((item) => {
-    let li = document.createElement("li");
-    li.setAttribute(item.bind, item.id);
-    li.textContent = list;
-    ul.appendChild(li);
-  });
-}
-
-function li(object, type) {
-  const li = document.createElement("li");
-  li.setAttribute(`${type}-id`, object.id);
-  li.textContent = getName(object, type);
-  return li;
-}
-
 export function subject(parent, subjects) {
   clearParent(parent);
 
   subjects.forEach((subject) => {
     const ele = li(subject, "subject");
+    const data = span(subject);
 
-    const span = document.createElement("span");
-    span.setAttribute(subject.bind, subject.id);
-    span.textContent = subject.data;
-    ele.appendChild(span);
-
+    bind(ele, subject);
+    ele.appendChild(data);
     if (subject.observerList.length !== 0) {
       ele.appendChild(sub(subject.observerList));
     }
@@ -49,20 +31,73 @@ export function subjectList(parent, subjects) {
   clearParent(parent);
 }
 export function observerList(parent, observers) {}
+
 function clearParent(parent) {
   return (parent.textContent = "");
 }
+
 function getName(object, type) {
   return object.name + (object.name === type ? `@${object.id}` : "");
+}
+function sub(list, type) {
+  const ul = document.createElement("ul");
+  list.forEach((item) => {
+    const ele = li(item, list);
+    bind(ele, item);
+    if (type === "observer") {
+      const data = span(item.getData());
+      bind(data, item);
+    }
+    ul.appendChild(ele);
+  });
+}
+
+function li(object, type) {
+  const li = document.createElement("li");
+  li.textContent = getName(object, type);
+  return li;
+}
+
+function span(subject) {
+  const span = document.createElement("span");
+  span.textContent = subject.data;
+  return span;
+}
+
+function bind(ele, item) {
+  return ele.setAttribute(item.bind, item.id);
+}
+
+export function select(type, parent, items, events) {
+  clearParent(parent);
+
+  const option = optionTitle(type);
+  parent.appendChild(option);
+
+  items.forEach(item => {
+    const option = options(item, type);
+    option.addEventListener(events.click, events.change);
+    parent.appendChild(option);
+  });
+}
+function options(item, type) {
+  const option = document.createElement('option');
+  option.value = item.id;
+  option.textContent = getName(item, type);
+  return option;
+}
+function optionTitle(type) {
+  const option = document.createElement("option");
+  option.textContent = `${type}s`;
+  option.disabled = true;
+  option.selected = true;
+  return option;
 }
 
 function populateSelect(items) {
   const select = document.getElementById("selectSubject");
   select.textContent = "";
-  const defaultOption = document.createElement("option");
-  defaultOption.textContent = "Subjects";
-  defaultOption.disabled = true;
-  defaultOption.selected = true;
+
   select.appendChild(defaultOption);
   items.forEach((item) => {
     const option = document.createElement("option");
@@ -75,26 +110,7 @@ function populateSelect(items) {
     });
   });
 }
-function actions(subject) {
-  const options = document.getElementById("subjectOptions");
-  options.textContent = "";
-  const label = document.createElement("label");
-  label.htmlFor = "updateData";
-  label.textContent = `Update the Data for "${subject.name}"`;
-  const input = document.createElement("input");
-  input.name = "updateData";
-  input.value = subject.getData();
 
-  const button = document.createElement("button");
-  button.type = "submit";
-  button.textContent = "Update";
-
-  options.appendChild(label);
-  options.appendChild(input);
-  options.appendChild(button);
-
-  getObservers(subject, T.getObjects("observer"));
-}
 function getObservers(subject, observers) {
   console.log(observers);
 }
