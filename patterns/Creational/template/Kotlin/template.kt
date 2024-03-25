ebcf5db3f40e8121e0281b398c1dda63c017f814
abhiram-shaji <122574760+abhiram-shaji@users.noreply.github.com>
2024-03-25 00:43:05 -0700

    Add the concrete class implementation

--- patterns/Creational/template/Kotlin/template.kt
+++ patterns/Creational/template/Kotlin/template.kt
@@ -11,3 +11,24 @@ abstract class NamesTemplate {
     abstract fun checkName(name: String)
     abstract fun listNames()
 }
+
+class NamesDatabase : NamesTemplate() {
+    private val namesList = mutableListOf<String>()
+
+    override fun addName(name: String) {
+        namesList.add(name)
+        println("Added '$name' to the database.")
+    }
+
+    override fun checkName(name: String) {
+        if (namesList.contains(name)) {
+            println("Checked: '$name' exists in the database.")
+        } else {
+            println("Checked: '$name' does not exist in the database.")
+        }
+    }
+
+    override fun listNames() {
+        println("Current names in the database: $namesList")
+    }
+}
