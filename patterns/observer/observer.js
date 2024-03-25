9eda79030314be9a064afae94564d0c7d258539d
swathysukumaran <swathysukumaran@gmail.com>
2024-03-24 20:33:17 -0700

    Add feature to restrict duplicate subscription

--- patterns/observer/observer.js
+++ patterns/observer/observer.js
@@ -14,19 +14,33 @@ class Subject {
         // The list of subscribers
         this.subscribersList = []
     }
-
+    // Function to check if observer is already subscribed
+    isSubscribed(fn) {
+        return this.subscribersList.includes(fn);
+    }
     // Function is added to the list of subscribers
     subscribe(fn) {
-        this.subscribersList.push(fn);
+        if (!this.isSubscribed(fn)) {
+            this.subscribersList.push(fn);
+            console.log("Observer subscribed successfully");
+        } else {
+            console.log("Observer is already subscribed");
+        }
     }
 
     // Function will get removed from the subscribers list
     unsubscribe(fn) {
-        this.subscribersList = this.subscribersList.filter((item) => item !== fn);
+        if (this.isSubscribed(fn)) {
+            this.subscribersList = this.subscribersList.filter(item => item !== fn);
+            console.log("Observer unsubscribed successfully");
+        } else {
+            console.log("Observer is not subscribed");
+        }
     }
 
     // To broadcast to all the observers
     broadcast(data) {
+        console.log("Broadcasting message to all subscribers...");
         for (let i = 0; i < this.subscribersList.length; i++) {
             // Calling each function in the observers list with the broadcast data
             this.subscribersList[i](data);
@@ -44,6 +58,14 @@ const fn = (data) => {
 // function 'fn' subscribed to Observable
 subject.subscribe(fn);
 
+// Trying to subscribe the same observer again
+subject.subscribe(fn);
 
 // Broadcasting message to all the subscribers
 subject.broadcast("Hello from subject");
+
+// Unsubscribing the observer
+subject.unsubscribe(fn);
+
+// Trying to unsubscribe an observer which is not subscribed
+subject.unsubscribe(fn);
