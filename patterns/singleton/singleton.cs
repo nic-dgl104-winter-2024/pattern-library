81d0eb6648eb455ecce9259c67d4a2bf4ddf1481
kuljit95 <144160996+kuljit95@users.noreply.github.com>
2024-03-26 14:29:18 -0700

    Update the class name
    Here, I update the class name before the instance according to the feedback. Now, I have fixed all the errors.

--- patterns/singleton/singleton.cs
+++ patterns/singleton/singleton.cs
@@ -1,17 +1,17 @@
 public sealed class Calculations
 {
-    private static readonly Calculationsinstance = new Calculations();
+    private static readonly Calculations Calculationsinstance = new Calculations();
     static Calculations()
     {
     }
     private Calculations()
     {
     }
-    public static CalculationsInstance
+    public static Calculations CalculationsInstance
     {
         get
         {
-            return instance;
+            return Calculationsinstance;
         }
     }
     // Add method for performing addition
@@ -26,13 +26,13 @@ class Program
     static void Main(string[] args)
     {
         // Access the Singleton instance
-        Calculations cals = Calculations.Instance;
+        Calculations cals = Calculations.CalculationsInstance;
 
         // Perform addition operation
         int result = cals .Add(5, 3);
 
         Console.WriteLine("Result of addition: " + result);
-        Calculations cals1 = Calculations.Instance;
+        Calculations cals1 = Calculations.CalculationsInstance;
          if(cals1 == cals)
 	    {
              Console.WriteLine("cals1 and cals both instances are equal");
