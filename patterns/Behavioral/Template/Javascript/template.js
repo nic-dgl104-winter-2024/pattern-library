0bde2ba7febc8901a20614d8a4475084ce09ed97
ting-dev-coder <missjoker59@gmail.com>
2024-03-26 00:10:15 -0700

    [add] JavaScript implementation for Template pattern

---
+++ patterns/Behavioral/Template/Javascript/template.js
@@ -0,0 +1,92 @@
+/**
+ * The concept was referred from https://refactoring.guru/
+design-patterns/template-method
+ * This example simply demonstrates how to implement the
+ * Template Design Pattern in a scenario: imagine you are
+ * standing in front of a kiosk machine at McDonald's to
+ * register, and you will receive a card in return.
+ */
+
+class CommunicateWithDevice {
+	constructor() {}
+	on() {
+		throw new Error("Subclasses must override the `on` method");
+	}
+	off() {
+		throw new Error("Subclasses must override the `off` method");
+	}
+	process() {
+		throw new Error("Subclasses must override the `process` method");
+	}
+}
+
+class CardDispenser extends CommunicateWithDevice {
+	on() {
+		console.log("card dispenser on");
+	}
+	off() {
+		console.log("card dispenser off");
+	}
+	process() {
+		this.checkRemainCardQuantity();
+		this.writeByteToCard();
+		this.dispenseCard();
+	}
+	checkRemainCardQuantity() {
+		console.log("check quantity....");
+	}
+	writeByteToCard() {
+		console.log("writing....");
+	}
+	dispenseCard() {
+		console.log("dispensing...");
+	}
+}
+
+class BillAcceptor extends CommunicateWithDevice {
+	on() {
+		console.log("card dispenser on");
+	}
+	off() {
+		console.log("card dispenser off");
+	}
+	process() {
+		this.collectBills();
+		this.countBills();
+	}
+	collectBills() {
+		console.log("Collecting bills...");
+	}
+	countBills() {
+		console.log("Counting bills...");
+	}
+}
+class MembershipCard {
+	#cardDispenser = null;
+	#billAcceptor = null;
+
+	constructor(cardDispenser, billAcceptor) {
+		this.#cardDispenser = cardDispenser;
+		this.#billAcceptor = billAcceptor;
+	}
+
+	initialize() {
+		this.#billAcceptor.on();
+		this.#cardDispenser.on();
+	}
+	getPhysicalCard() {
+		this.#billAcceptor.process();
+		this.#billAcceptor.off();
+
+		this.#cardDispenser.process();
+		this.#cardDispenser.off();
+	}
+}
+
+const cardDispenser = new CardDispenser();
+const billAcceptor = new BillAcceptor();
+const memberOne = new MembershipCard();
+const membershipCardCreator = new MembershipCardCreator(cardDispenser, billAcceptor);
+
+membershipCardCreator.initialize();
+membershipCardCreator.getPhysicalCard();
