d258c430e1c55650f092c62e75c285053a490f0b
alinaghale88 <alinaghale.official@gmail.com>
2024-03-26 18:06:49 -0700

    Define generic delegate for the event handler

--- patterns/observers/observer-delegate.cs
+++ patterns/observers/observer-delegate.cs
@@ -7,3 +7,7 @@ https://www.c-sharpcorner.com/article/design-pattern-with-net-delegates/
 https://www.devleader.ca/2023/11/17/examples-of-the-observer-pattern-in-c-how-to-simplify-event-management/#google_vignette
 */
 
+using System;
+
+// Define the generic delegate for the event handler
+public delegate void EventHandler<T>(T data);
