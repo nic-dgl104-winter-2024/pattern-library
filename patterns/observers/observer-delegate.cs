b0fb208be613c58aed2cb20c90f0bac8f85ea3e2
alinaghale88 <alinaghale.official@gmail.com>
2024-03-26 17:59:46 -0700

    Add comments for references & code implementation

---
+++ patterns/observers/observer-delegate.cs
@@ -0,0 +1,9 @@
+// Generic Delegate Implementation of the Observer Pattern
+
+/*
+References used: 
+https://www.youtube.com/watch?v=yoAOo4dM2pk
+https://www.c-sharpcorner.com/article/design-pattern-with-net-delegates/
+https://www.devleader.ca/2023/11/17/examples-of-the-observer-pattern-in-c-how-to-simplify-event-management/#google_vignette
+*/
+
