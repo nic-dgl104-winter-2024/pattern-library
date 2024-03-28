1fa969e097316807eba79ee29f33eda2e6418936
abhiram-shaji <122574760+abhiram-shaji@users.noreply.github.com>
2024-03-27 17:24:59 -0700

    Refactor namestemplate and namesdatabase for improved feedback and control

--- patterns/Creational/template/Kotlin/template.kt
+++ patterns/Creational/template/Kotlin/template.kt
@@ -3,37 +3,36 @@ abstract class NamesTemplate {
     // Template method to manage names
     fun manageNames(name: String) {
         println("Starting to manage the name: $name")
-        addName(name)
-        checkName(name)
-        listNames()
+        val response = addName(name)
+        if (response) {
+            println("$name added successfully. Names in DB " + listNames())
+        } else {
+            println("Failed to add the $name")
+        }
         println("Finished managing the name: $name\n")
     }
 
     // Steps to be implemented by subclasses
-    abstract fun addName(name: String)
-    abstract fun checkName(name: String)
-    abstract fun listNames()
+    abstract fun addName(name: String): Boolean
+    abstract fun checkName(name: String): Boolean
+    abstract fun listNames(): MutableList<String>
 }
 
 // Concrete class implementing the steps for the names database
 class NamesDatabase : NamesTemplate() {
     private val namesList = mutableListOf<String>()
 
-    override fun addName(name: String) {
+    override fun addName(name: String): Boolean {
         namesList.add(name)
-        println("Added '$name' to the database.")
+        return checkName(name)
     }
 
-    override fun checkName(name: String) {
-        if (namesList.contains(name)) {
-            println("Checked: '$name' exists in the database.")
-        } else {
-            println("Checked: '$name' does not exist in the database.")
-        }
+    override fun checkName(name: String): Boolean {
+        return namesList.contains(name)
     }
 
-    override fun listNames() {
-        println("Current names in the database: $namesList")
+    override fun listNames(): MutableList<String> {
+        return namesList.toMutableList()
     }
 }
 
