d9c8428f6313cf447c4189718df56ccf373c6bb0
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2024-03-28 10:30:48 -0700

    Create the abstarct class with the template method

--- CandyFactory.java
+++ CandyFactory.java
@@ -1,3 +1,17 @@
+abstract class  CandyMaker {
+
+    public final void makeCandy() {
+        prepareIngredients();
+        moldShape();
+        wrapCandy();
+    }
+    
+}
+
+
+
+
+
 public class CandyFactory {
     public static void main(String[] args) {
         System.out.println("Making candy");
